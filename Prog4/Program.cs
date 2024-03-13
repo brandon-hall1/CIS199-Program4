8fdc93c8551aca04c292338b13df4a32ff953b6f
unknown <brandonthall3@gmail.com>
2024-03-12 20:48:40 -0400

    first commit

---
+++ Prog4/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Grading ID: K3194
+ * Program 4
+ * Due Date: 4/18/2022
+ * Course Section: CIS 199-01
+ * Description: Creates 5 Superhero objects, modifies them, and prints them out before and after object modification
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    class Program
+    {
+        //Precondition: None (Program Entry)
+        //Postcondition: Program ends execution (Program Exit)
+        static void Main(string[] args)
+        {
+            Superhero sh1 = new Superhero("John", "Bardstown", "Invisibility", 2001, "Laser Eyes"); //Create 5 superhero objects
+            Superhero sh2 = new Superhero("Tyler", "Louisville", "Super Speed", 1994, "Underwater Breathing");
+            Superhero sh3 = new Superhero("Jackson", "Nashville", "Super Jumping", 1900, "Immortal");
+            Superhero sh4 = new Superhero("Preston", "Chicago", "Flexible Limbs", 1983, "Magnetic");
+            Superhero sh5 = new Superhero("Carrie", "New York City", "Immortal", 1999, "Super Strength");
+
+            Superhero[] sHeroArray = { sh1, sh2, sh3, sh4, sh5 }; //Create an array to hold the 5 unique Superhero objects
+
+            printSuperheroes(sHeroArray); //Call printSuperheroes function passing in a Superhero array as a parameter
+
+            sh1.FirstSuperpower = "Telekinesis"; //Modify superhero fields
+            sh2.SecondSuperpower = "Extreme Flexibility";
+            sh3.FirstSuperpower = "Unlimited Endurance";
+            sh4.SecondSuperpower = "Fire Casting";
+            sh5.FirstSuperpower = "High Intelligence";
+            sh2.OffPlanetEarth();
+            sh4.OffPlanetEarth();
+
+            printSuperheroes(sHeroArray);
+
+            sh1.OffPlanetEarth(); //Set the rest of the superheros to Off Planet Earth status
+            sh3.OffPlanetEarth();
+            sh5.OffPlanetEarth();
+
+            printSuperheroes(sHeroArray);
+        }
+
+        //Preconditions: Must pass in an array of Superhero objects
+        //Postconditions: Superhero Info/Data Printed to Console
+        static void printSuperheroes(Superhero[] sHeroArray)
+        {
+            foreach(Superhero s in sHeroArray) //Loop through each Superhero in array
+            {
+                Console.WriteLine(s + "\n"); //Use overridden ToString to print Superhero details and add line break at end of each record for readability
+            }
+        }
+    }
+}
---
+++ Prog4/Superhero.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    class Superhero
+    {
+
+		//Precondition: Name, Birthcity, Superpower 1, Birthyear, and Superpower 2 must be passed as parameters
+		//Postcondition: Superhero object created with corresponding data populated
+		public Superhero(string name, string birthcity, string superpower1, int birthyear, string superpower2)
+		{
+			SuperheroName = name;
+			BirthCity = birthcity;
+			FirstSuperpower = superpower1;
+			BirthYear = birthyear;
+			SecondSuperpower = superpower2;
+		}
+
+		private int _year; //private int to hold Birthyear; accessed through BirthYear
+		public const int DEFAULT_YEAR = 1999; //public const int to hold the default birthyear
+
+		private bool earthStatus = true; //boolean to hold superhero's on Earth status; default is true (on earth)
+
+		public string SuperheroName //String for superhero name
+		{
+			get; set;
+		}
+
+		public string BirthCity //String for superhero birth city
+		{
+			get; set;
+		}
+
+		public string FirstSuperpower //String for superhero's first superpower
+		{
+			get; set;
+		}
+
+		public int BirthYear //int for superhero's birth year
+		{
+			//Precondition: BirthYear property accessed from Superhero object
+			//Postcondition: _year returned to calling statement
+			get
+			{
+				return _year;
+			}
+
+			//Precondition: BirthYear property is set to a valid integer
+			//Postcondition: _year set to inputted value if positive, or set to DEFAULT_YEAR if negative
+			set
+			{
+				if (value >= 0) //If inputted year is positive, set the year to the inputted value
+				{
+					_year = value;
+				}
+				else
+				{
+					_year = DEFAULT_YEAR; //If inputted year is negative, set the Birthyear to the default year value
+				}
+			}
+		}
+
+		public string SecondSuperpower //String for superhero's second superpower
+		{
+			get; set;
+		}
+
+		public char Initial //Read Only Char property for superhero's first initial
+		{
+			//Precondition: SuperheroName property is set to a valid string
+			//Postcondition: First letter from SuperheroName returned to calling statement
+			get
+			{
+				return SuperheroName[0];
+			}
+		}
+
+		//Precondition: Superhero object instantiated, called directly from object
+		//Postcondition: Superhero's earthStatus set to true
+		public void OnPlanetEarth()
+		{
+			earthStatus = true;
+		}
+
+		//Precondition: Superhero object instantiated, called directly from object
+		//Postcondition: Superhero's earthStatus set to false
+		public void OffPlanetEarth()
+		{
+			earthStatus = false;
+		}
+
+		//Precondition: Superhero object instantiated, called directly from object
+		//Postcondition: Superhero's earthStatus is returned to calling statement
+		public bool IsOnPlanetEarth()
+		{
+			return earthStatus;
+		}
+
+		//Precondition: Superhero object instantiated, called directly from object
+		//Postcondition: Returns string with all necessary Superhero info with formatting
+		override public string ToString()
+		{
+			return $"Name: {SuperheroName}{Environment.NewLine}Birth City: {BirthCity}{Environment.NewLine}First Superpower: {FirstSuperpower}{Environment.NewLine}Birth Year: {BirthYear}{Environment.NewLine}Second Superpower: {SecondSuperpower}{Environment.NewLine}On Planet Earth: {IsOnPlanetEarth()}";
+		}
+	}
+}
